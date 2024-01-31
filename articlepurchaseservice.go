d7af85b375835d32f5f6ff6fdb57928f390ae816
PedroVianaBlueRiver <pedro.viana@blueriver.com>
2024-01-30 19:15:17 -0600

    update message

--- articlepurchaseservice.go
+++ articlepurchaseservice.go
@@ -13,13 +13,13 @@ func (ap *ArticlePurchaseResponse) PerformPurchasev4(quantity int) {
 		ap.issuccess = false
 		ap.response = fmt.Sprintln("There is no article available in stock")
 
-	case ap.at.quantity < quantity:
+	case ap.at.stock < quantity:
 		ap.issuccess = false
 		verb := "is"
 		if ap.at.stock > 1 {
 			verb = "are"
 		}
-		ap.response = fmt.Sprintf("There %v just %v available in stock", verb, ap.at.stock)
+		ap.response = fmt.Sprintf("There %v just %v (%v) available in stock", verb, ap.at.stock, ap.at.name)
 
 	default:
 		ap.at.date = time.Format("2006-01-02")
@@ -31,7 +31,40 @@ func (ap *ArticlePurchaseResponse) PerformPurchasev4(quantity int) {
 }
 
 func main() {
-	apresponse := NewArticlePurchaseResponse("TV", 3, 300)
-	apresponse.PerformPurchasev4(4)
-	fmt.Print(apresponse.response)
+	cont := 0
+	listPurchase := []struct {
+		name      string
+		stock     int
+		unitprice float64
+		quantity  int
+	}{
+		{
+			name:      "TV",
+			stock:     3,
+			unitprice: 300.50,
+			quantity:  4,
+		},
+		{
+			name:      "Phone",
+			stock:     3,
+			unitprice: 623.50,
+			quantity:  2,
+		},
+	}
+
+	for i := 0; i < len(listPurchase); i++ {
+		cont = cont + 1
+		fmt.Printf("Purchase #%v ---------------------------------------------------------------------------------------\n", cont)
+
+		apresponse1 := NewArticlePurchaseResponse(listPurchase[i].name, listPurchase[i].stock, listPurchase[i].unitprice)
+		apresponse1.PerformPurchasev4(listPurchase[i].quantity)
+		if apresponse1.issuccess {
+			fmt.Println(apresponse1.response)
+			fmt.Printf("Article: %v\n", apresponse1.at.name)
+			fmt.Printf("Quantity: %v\n", apresponse1.at.quantity)
+			fmt.Printf("Total : $ %2f\n", apresponse1.at.total)
+		} else {
+			fmt.Println(apresponse1.response)
+		}
+	}
 }
