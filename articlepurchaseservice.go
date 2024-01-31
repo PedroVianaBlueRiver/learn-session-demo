8cd3fdac3e46ff0f7be6980bfc7f907bbbc68092
PedroVianaBlueRiver <157846265+PedroVianaBlueRiver@users.noreply.github.com>
2024-01-31 14:59:17 -0600

    create new test (#2)

--- articlepurchaseservice.go
+++ articlepurchaseservice.go
@@ -11,7 +11,7 @@ func (ap *ArticlePurchaseResponse) PerformPurchasev4(quantity int) {
 	switch {
 	case ap.at.stock == 0:
 		ap.issuccess = false
-		ap.response = fmt.Sprintln("There is no article available in stock")
+		ap.response = fmt.Sprintf("There is no article available in stock")
 
 	case ap.at.stock < quantity:
 		ap.issuccess = false
@@ -19,7 +19,7 @@ func (ap *ArticlePurchaseResponse) PerformPurchasev4(quantity int) {
 		if ap.at.stock > 1 {
 			verb = "are"
 		}
-		ap.response = fmt.Sprintf("There %v just %v (%v) available in stock", verb, ap.at.stock, ap.at.name)
+		ap.response = fmt.Sprintf("There %v just %v(%v) available in stock", verb, ap.at.stock, ap.at.name)
 
 	default:
 		ap.at.date = time.Format("2006-01-02")
