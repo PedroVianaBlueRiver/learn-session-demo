824c6cab943198e7ac6000f557d285d2bfc023fb
PedroVianaBlueRiver <pedro.viana@blueriver.com>
2024-02-06 12:14:38 -0600

    create createPurchase

--- maparticlepurchaseservice.go
+++ maparticlepurchaseservice.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // GetItem returns the quantity of item
 func GetItem(item map[int]ArticleModel, id int) (ArticleModel, bool, string) {
@@ -12,6 +15,7 @@ func GetItem(item map[int]ArticleModel, id int) (ArticleModel, bool, string) {
 	return item[id], true, ""
 }
 
+// Add Item of the map
 func AddItem(item map[int]ArticleModel, stock, id int, name string, unitprice float64) map[int]ArticleModel {
 	at := ArticleModel{name: name, unitprice: unitprice, stock: stock}
 	item[id] = at
@@ -19,6 +23,7 @@ func AddItem(item map[int]ArticleModel, stock, id int, name string, unitprice fl
 	return item
 }
 
+// Delete item of the map
 func DeleteItem(item map[int]ArticleModel, id int) (map[int]ArticleModel, bool, string) {
 	if _, ok := item[id]; !ok {
 		return item, false, fmt.Sprintf("article with id: %v doesn't exist", id)
@@ -27,6 +32,7 @@ func DeleteItem(item map[int]ArticleModel, id int) (map[int]ArticleModel, bool, 
 	return item, true, ""
 }
 
+// update item of the map
 func updateItem(item map[int]ArticleModel, id, stock int, name string, unitprice float64) (map[int]ArticleModel, bool, string) {
 	if _, ok := item[id]; !ok {
 		return item, false, fmt.Sprintf("article with id: %v doesn't exist", id)
@@ -37,12 +43,41 @@ func updateItem(item map[int]ArticleModel, id, stock int, name string, unitprice
 	at.stock = stock
 	at.name = name
 	at.unitprice = unitprice
+	item[id] = at
 
 	return item, true, ""
 }
 
+// create a purchaseModel and update the map[int]ArticleModel after performing a purchase
+func createPurchase(id, quantity int) (map[int]ArticleModel, PurchaseModels, bool, string) {
+	time := time.Now()
+	lstAM := NewListMapArticle()
+	fmt.Println("Initial Map Article: ", lstAM)
+
+	itemAM, ok, msn := GetItem(lstAM, id)
+
+	if !ok {
+		return map[int]ArticleModel{}, PurchaseModels{}, false, msn
+	}
+
+	itemAM.stock = itemAM.stock - quantity
+
+	totalpurchase := itemAM.unitprice * float64(quantity)
+	lstAM, ok1, msn1 := updateItem(lstAM, id, itemAM.stock, itemAM.name, itemAM.unitprice)
+
+	if !ok1 {
+		return map[int]ArticleModel{}, PurchaseModels{}, false, msn1
+	}
+
+	purchasemodel := NewListMapPruchase(id, quantity, time.Format("2006-01-02"), totalpurchase)
+
+	return lstAM, *purchasemodel, true, ""
+
+}
+
 func main() {
 	articleList := NewListMapArticle()
+	fmt.Println("************************************************* GetItem() *******************************************************************")
 	fmt.Println("GetItem(1) Initial Map: ", articleList)
 	at, ok, msn := GetItem(articleList, 1)
 	if !ok {
@@ -52,10 +87,11 @@ func main() {
 		fmt.Println("Article stock: ", at.stock)
 		fmt.Println("Article price: ", at.unitprice)
 	}
-
+	fmt.Println("************************************************* AddItem() *******************************************************************")
 	articleList = AddItem(articleList, 400, 6, "CPU", 1300)
 	fmt.Println("AddItem() Updated Map: ", articleList)
 
+	fmt.Println("************************************************* updateItem() *******************************************************************")
 	articleList, ok, msn = updateItem(articleList, 2, 20, "Phone", 310)
 
 	if !ok {
@@ -64,6 +100,7 @@ func main() {
 		fmt.Println("updateItem() Updated Map: ", articleList)
 	}
 
+	fmt.Println("************************************************* DeleteItem() *******************************************************************")
 	articleList, ok, msn = DeleteItem(articleList, 1)
 	if !ok {
 		fmt.Println(msn)
@@ -71,4 +108,12 @@ func main() {
 		fmt.Println("DeleteItem() Updated Map: ", articleList)
 	}
 
+	fmt.Println("************************************************* createPurchase() *******************************************************************")
+	mapresponse, purchaseresponse, ok2, msn2 := createPurchase(1, 2)
+	if !ok2 {
+		fmt.Println(msn2)
+	} else {
+		fmt.Println("Updated Map: ", mapresponse)
+		fmt.Println("purchase: ", purchaseresponse)
+	}
 }
